9e03559d5cef22f8d3055bf1c7b8628a9ed263b8
c786909486 <786909486@qq.com>
2018-11-06 20:16:01 +0800

    sss

--- app/src/main/java/com/axun/bluetoothsendtest/BluetoothListActivity.kt
+++ app/src/main/java/com/axun/bluetoothsendtest/BluetoothListActivity.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import android.os.Bundle
 import android.support.annotation.RequiresApi
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.inuker.bluetooth.library.BluetoothClient
 import com.inuker.bluetooth.library.search.SearchRequest
@@ -13,6 +14,8 @@ import kotlinx.android.synthetic.main.activity_bluetooth_list.*
 
 class BluetoothListActivity :AppCompatActivity() {
     private lateinit var mClient: BluetoothClient
+    private lateinit var arrayAdapter: ArrayAdapter<String>
+    private var items:ArrayList<String> = arrayListOf()
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +24,8 @@ class BluetoothListActivity :AppCompatActivity() {
         ToolbarUtils.setToolbar(this,tool_bar)
         ToolbarUtils.addMiddleTitle(this,"搜索蓝牙",tool_bar)
         mClient = BluetoothClient(this)
+        arrayAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,android.R.id.text1,items)
+        lv_item.adapter = arrayAdapter
         searchBluttooth()
     }
 
@@ -29,7 +34,7 @@ class BluetoothListActivity :AppCompatActivity() {
                 .searchBluetoothLeDevice(3000, 3)   // 先扫BLE设备3次，每次3s
                 .searchBluetoothClassicDevice(5000) // 再扫经典蓝牙5s
                 .searchBluetoothLeDevice(2000)      // 再扫BLE设备2s
-                .build();
+                .build()
         mClient.search(request,object :SearchResponse{
             override fun onSearchStopped() {
                 showToast("停止搜索")
@@ -40,7 +45,10 @@ class BluetoothListActivity :AppCompatActivity() {
             }
 
             override fun onDeviceFounded(device: SearchResult?) {
-                showToast(device!!.name)
+                if (!items.contains(device!!.name+device.address)){
+                    items.add(device!!.name+device.address)
+                }
+                arrayAdapter.notifyDataSetChanged()
             }
 
             override fun onSearchCanceled() {
